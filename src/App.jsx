242f506c2538083f1de774303cb555f8beb6acd4
Abubakar1010 <abubakar850772@gmail.com>
2024-03-15 06:09:29 +0600

    project initialized with react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className='text-3xl font-bold text-orange-600 text-center'>Project Setup With react and tailwind</h1>
+
+    </>
+  )
+}
+
+export default App
